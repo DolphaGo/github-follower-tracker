77a2524902a553c20ac6b5e0ada5e027a9f3d09d
dolphago <adamdoha@naver.com>
2021-03-15 04:55:59 +0900

    Refactor : Delete unnecessary skills

--- src/main/java/me/dolphago/controller/FollowController.java
+++ src/main/java/me/dolphago/controller/FollowController.java
@@ -5,8 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import me.dolphago.service.FollowTrackingService;
@@ -20,17 +18,6 @@ public class FollowController {
     @GetMapping("/check/{handle}")
     public ResponseEntity<String> check(@PathVariable("handle") String handle) {
         log.info("유저 {}의 팔로우 상태를 점검합니다.", handle);
-        log.info("내가 팔로잉하고 있는 {}명 저장", followTrackingService.saveFollowings(handle));
-        log.info("나를 팔로우하고 있는 {}명 저장", followTrackingService.saveFollowers(handle));
         return ResponseEntity.ok(followTrackingService.checkFollow(handle));
     }
-
-    @GetMapping("/check/{handle}")
-    public ResponseEntity<String> check(@PathVariable("handle") String handle) {
-        log.info("유저 {}의 팔로우 상태를 점검합니다.", handle);
-        log.info("내가 팔로잉하고 있는 {}명 저장", followTrackingService.saveFollowings(handle));
-        log.info("나를 팔로우하고 있는 {}명 저장", followTrackingService.saveFollowers(handle));
-        return ResponseEntity.ok(followTrackingService.checkFollow(handle));
-    }
-
 }
